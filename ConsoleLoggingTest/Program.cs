765d812c06bf3d65fb73de53d573890049aa28f4
Adam Hathcock <adam.hathcock@autodesk.com>
2015-12-01 09:01:24 +0000

    Sample to show a NullReferenceException

---
+++ ConsoleLoggingTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace ConsoleLoggingTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddLogging();
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            var factory = serviceProvider.GetService<ILoggerFactory>();
+            factory.AddConsole();
+
+            var logger = factory.CreateLogger<Program>();
+            logger.LogError("test");
+
+            Console.ReadLine();
+        }
+    }
+}
